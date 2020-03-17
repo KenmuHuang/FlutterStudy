97e905d08f662b81ae0e0fb073a8b1b2c8026e0d
huangjianwu <kenmuhuang@xinli001.com>
2020-03-18 00:25:40 +0800

    [feat] How do I access UserDefault in Flutter?, https://pub.dev/packages/shared_preferences#-example-tab-

--- lib/demo/cache_demo.dart
+++ lib/demo/cache_demo.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -26,9 +28,29 @@ class CacheDemoPage extends StatefulWidget {
 }
 
 class _CacheDemoPageState extends State<CacheDemoPage> {
+  Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+  Future<int> _counter;
+  final String _counterKey = 'counter';
+
+  Future<void> _incrementCounter() async {
+    final SharedPreferences prefs = await _prefs;
+    final int counter = (prefs.getInt(_counterKey) ?? 0) + 1;
+    print('Pressed $counter times.');
+
+    setState(() {
+      _counter = prefs.setInt(_counterKey, counter).then((bool success) {
+        return counter;
+      });
+    });
+  }
+
   @override
   void initState() {
     super.initState();
+
+    _counter = _prefs.then((SharedPreferences prefs){
+      return (prefs.getInt(_counterKey) ?? 0);
+    });
   }
 
   @override
@@ -43,19 +65,32 @@ class _CacheDemoPageState extends State<CacheDemoPage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: RaisedButton(
-          onPressed: _incrementCounter,
-          child: Text('Increment Counter'),
+        child: FutureBuilder<int>(
+          future: _counter,
+          builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.waiting: {
+                return const CircularProgressIndicator();
+              }
+              default: {
+                if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                } else {
+                  return Text(
+                    'Button tapped ${snapshot.data} time${snapshot.data == 1 ? '' : 's'}.\n\n'
+                        'This should persist across restarts.',
+                  );
+                }
+              }
+            }
+          },
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
     );
-  }
-
-  void _incrementCounter() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    int counter = (prefs.getInt('counter') ?? 0) + 1;
-    print('Pressed $counter times.');
-
-    await prefs.setInt('counter', counter);
   }
 }
